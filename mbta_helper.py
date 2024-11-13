e313b4c61e2c65c0ae20acf2f612967803f498ed
Cole Davenport <eloc142@me.com>
2024-11-12 23:39:43 -0500

    getting stop resonces

--- mbta_helper.py
+++ mbta_helper.py
@@ -46,10 +46,12 @@ def get_lat_lng(place_name: str) -> tuple[str, str]:
        # pprint.pprint(response_data)
         points = response_data['features'][0]['geometry']['coordinates']
         print(points)
-        latitude = points[0]
-        longitude = points[1]
-        #print(longitude)
-    return (latitude, longitude)
+        # FOR SOME REASON THE POINTS ARE WRONG (LONG GOES BEFORE LAT SHOULD BE INVERSE)
+
+        latitude = points[1]
+        longitude = points[0]
+        print(latitude, longitude)
+    return latitude, longitude
 
 
 def get_nearest_station(latitude: float, longitude: float) -> tuple[str, bool]:
@@ -58,19 +60,30 @@ def get_nearest_station(latitude: float, longitude: float) -> tuple[str, bool]:
 
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
     """
-    query = f"/stops?filter[latitude]={latitude}&filter[longitude]={longitude}&filter "
-    
-    url=f"{MBTA_BASE_URL}{query}.json?access_token={MBTA_API_KEY}&types=poi"
+    radius = 100
+    query = f"?filter[latitude]={latitude}&filter[longitude]={longitude}/sort=-distance"
+    url=f"{MBTA_BASE_URL}{query}=access_token={MBTA_API_KEY}"
     print(url) # Try this URL in your browser first
 
     with urllib.request.urlopen(url) as resp:
         response_text = resp.read().decode("utf-8")
         response_data = json.loads(response_text)
-        pprint.pprint(response_data)
+       # stop = response_data['features'][0]['geometry']['coordinates']
+        #pprint.pprint(response_data)
     #   points = response_data['features'][0]['geometry']['coordinates']
-       # print(points)
-       
-    return
+       # print(points)b
+        if "data" in response_data:
+            stop  = response_data["data"][0]
+            #print (stop)
+            station = stop["attributes"]["name"]
+            wheelchair = stop["attributes"]["wheelchair_boarding"]
+            if wheelchair == 1:
+                wheelchair = "has wheelchair boarding"
+            if wheelchair == 0:
+                wheelchair = "does not have wheelchair boarding"
+            #print (station)
+            #print (wheelchair)
+            return station, wheelchair
 
 
 def find_stop_near(place_name: str) -> tuple[str, bool]:
@@ -87,10 +100,10 @@ def main():
     You should test all the above functions here
     """
     place_name = input("Enter the name of your place:")
-    coords = get_lat_lng(place_name)
-    print (coords)
-    get_nearest_station(coords)
-    pass
+    latitude, longitude = get_lat_lng(place_name)
+    response = get_nearest_station(latitude, longitude)
+    print (response)
+    return response
 
 
 if __name__ == "__main__":
